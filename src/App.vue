fb4a6c7e850767e84f547bbe24b24cf1026e5bde
Dmitry Yudin <dmitriy.yudin@abrigo.com>
2019-09-17 14:06:26 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app class="grey lighten-4">
+    <NavBar></NavBar>
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import NavBar from '@/components/NavBar'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
+    NavBar,
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav>
+        <v-toolbar flat app>
+
+            <v-toolbar-title class="text-uppercase grey--text">
+                <span class="font-weight-light">Todo</span>
+                <span>App</span>
+            </v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn text>
+                Sign Out
+                <v-icon right>mdi-exit-to-app</v-icon>
+            </v-btn>
+
+        </v-toolbar>
+    </nav>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+};
+</script>
