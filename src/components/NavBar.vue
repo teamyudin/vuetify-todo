bbb406ebd91eb2483c45db38cb462c13e471c52b
Dmitriy Yudin <dima@Dmitriys-MacBook-Pro.local>
2019-09-19 07:48:23 -0500

    menu

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -6,7 +6,7 @@
                 <v-avatar size="100" > 
                     <img class="text-lg-center" src="https://api.adorable.io/avatars/188/abott@adorable.png'">
                 </v-avatar>
-                
+
                 <v-subheader>Dmitry Yudin</v-subheader>
             </v-layout>
 
@@ -33,6 +33,24 @@
             </v-toolbar-title>
 
             <v-spacer></v-spacer>
+
+            <v-menu offset-y>
+                <template v-slot:activator="{ on }">
+                    <v-btn text v-on="on">
+                        <v-icon left>mdi-chevron-down</v-icon>
+                        <span>Menu</span>
+                    </v-btn>
+                </template>
+                <v-list>
+                    <v-list-item
+                    v-for="link in links"
+                    :key="link.text"
+                    router :to="link.route"
+                    >
+                    <v-list-item-title >{{ link.text }}</v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
 
             <v-btn text>
                 Sign Out
