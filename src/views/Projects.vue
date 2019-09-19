b4cfcbe4a984b5162263ac95a01bb759c9421881
Dmitriy Yudin <dima@Dmitriys-MacBook-Pro.local>
2019-09-18 23:30:44 -0500

    projects

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -3,7 +3,25 @@
     <v-subheader>Projects</v-subheader>
 
     <v-container class="my-5">
-        content
+      <v-expansion-panels>
+          <v-expansion-panel
+            v-for="project in mine"
+            :key="project.title"
+          >
+            <v-expansion-panel-header>
+              {{ project.title }}
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              
+              <v-card>
+                <v-card-text class="px-4 grey--text">
+                  <div class="font-weight-bold">Due by: {{ project.due }}</div>
+                  <div>{{ project.content }}</div>
+              </v-card-text>
+              </v-card>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
     </v-container>
 
   </div>
@@ -12,7 +30,21 @@
 <script>
 
 export default {
-  components: {
+  data(){
+    return {
+      projects: [
+        { title: 'Design a new website', person: 'Dmitry Yudin', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Create a community forum', person: 'Dmitry Yudin', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+      ],
+    }
   },
+
+  computed:{
+    mine(){
+      return this.projects.filter(x => x.person === 'Dmitry Yudin');
+    }
+  }
 };
 </script>
